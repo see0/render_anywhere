6819eb675a747c715f89ddc16ce6f69f6e5cc774
Jason <see0@purdue.edu>
2014-01-21 23:18:56 -0800

    Render anywhere with wicked PDF

--- lib/render_anywhere.rb
+++ lib/render_anywhere.rb
@@ -8,7 +8,11 @@ module RenderAnywhere
   def render(*args)
     rendering_controller.render_to_string(*args)
   end
-  
+
+  def render_to_string_with_wicked_pdf(*args)
+    rendering_controller.render_to_string_with_wicked_pdf(*args)
+  end
+
   def rendering_controller
     @rendering_controller ||= RenderAnywhere::RenderingController.new
   end
